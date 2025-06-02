1d8e42fd89aa6eaaab5736864d91a2832b4342df
Hoai2801 <dreamhoaihack@gmail.com>
2025-06-02 14:45:49 +0700

    feature: upload file api

--- go.mod
+++ go.mod
@@ -16,9 +16,10 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.4 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -39,6 +40,6 @@ require (
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/postgres v1.5.11 // indirect
-	gorm.io/gorm v1.26.1 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
+	gorm.io/gorm v1.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"math/rand"
+	"github.com/google/uuid"
+	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -15,10 +19,53 @@ func main() {
 		})
 	})
 
+	router.GET("/file", func(context *gin.Context) {
+		entries, err := os.ReadDir("./tmp")
+		if err != nil {
+			fmt.Println("Error reading directory:", err)
+			return
+		}
+
+		count := 0
+		for _, entry := range entries {
+			if !entry.IsDir() {
+				count++
+			}
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"count": count,
+		})
+	})
+
 	router.POST("/", func(c *gin.Context) {
-		num := rand.Intn(10000)
-		c.JSON(200, gin.H{
-			"message": num,
+		file, err := c.FormFile("file")
+
+		// The file cannot be received.
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "No file is received",
+			})
+			return
+		}
+
+		// Retrieve file information
+		extension := filepath.Ext(file.Filename)
+		// Generate random file name for the new uploaded file so it doesn't override the old file with same name
+		newFileName := uuid.New().String() + extension
+
+		time.Sleep(10 * time.Second)
+
+		// The file is received, so let's save it
+		if err := c.SaveUploadedFile(file, "./tmp/"+newFileName); err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Unable to save the file",
+			})
+			return
+		}
+
+		// File saved successfully. Return proper result
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Your file has been successfully uploaded.",
 		})
 	})
 	router.Run(":8080")
